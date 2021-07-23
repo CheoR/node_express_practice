3f31cf0690858fc975917eba798cc1c227ab950d
CheoR <cheo.roman+github@gmail.com>
2021-07-23 05:31:19 -0500

    feat: display messages.

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const messages = [
+	{
+		name: "cow",
+		message: "moo moo moo moo"
+	},
+	{
+		name: "pig",
+		message: "oink oink oink oink"
+
+	}
+];
+
 app.use(express.static(__dirname));
+
+app.get('/messages', (request, response) => {
+	response.send(messages);
+})
+
 const server = app.listen(3000, () => {
 	console.log(`Server listening on port ${server.address().port}`);
 });
